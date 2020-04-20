3b7a7f9dc36a4bec4e70f81f7f8c263cf07ac462
Inhesta <matheusinhesta@live.com>
2020-04-20 17:35:04 -0300

    Arquivos iniciais

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Schema::defaultStringLength(191);
+        date_default_timezone_set('America/Sao_Paulo');
+        \Carbon\Carbon::setLocale('pt_BR');
+
+        if ($this->app->environment() == 'local') {
+            $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
