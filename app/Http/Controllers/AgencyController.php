10bc9f5e437fd71a7f2e375ac7526af367fdc9de
Inhesta <matheusinhesta@live.com>
2020-05-23 02:55:25 -0300

    Rotas de informação e edição da agência do usuário

---
+++ app/Http/Controllers/AgencyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agency;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AgencyController extends Controller {
+
+    public function index(){
+        $agency = \Auth::user()->agency()->get(['id', 'name', 'timezone', 'lang', 'fare_url', 'phone', 'email', 'created_at', 'updated_at']);
+        return response()->json(compact('agency'), 200);
+    }
+
+    public function update(Request $request){
+
+        $agency = \Auth::user()->agency;
+
+        $validator = Validator::make($request->all(), [
+            'name'     => 'string|max:255',
+            'url'      => 'string|max:255',
+            'timezone' => 'string|max:100',
+            'lang'     => 'string|max:10',
+            'phone'    => 'string|max:50',
+            'fare_url' => 'string|max:255',
+            'email'    => 'string|max:150'
+        ]);
+
+        if($validator->fails())
+            return response()->json($validator->errors()->toJson(), 400);
+
+        try{
+            $agency->update($request->input());
+
+            return response()->json(['message' => 'Agency updated successfully'], 200);
+        } catch (\Exception $e){
+            return response()->json(['message' => 'The agency could not be updated. Try again'], 500);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return response()->json(['message' => 'Entrou na API']);
@@ -15,13 +13,23 @@ Route::post('forget-password', 'AuthController@forgetPassword');
 
 // Authenticated Routes
 Route::middleware(['jwt.verify'])->group(function () {
+
+    // Auth
     Route::post('logout', 'AuthController@logout');
     Route::get('refresh', 'AuthController@refresh');
     Route::get('me', 'AuthController@getAuthenticatedUser');
     Route::post('change-password', 'AuthController@changePassword');
+
+    // Only admins
+    Route::middleware(['permissions.verify'])->group(function () {
+
+        Route::prefix('agency')->group(function(){
+            Route::get('', 'AgencyController@index');
+            Route::post('', 'AgencyController@update');
+        });
+
+
+//Route::resource('agency', 'AgencyController')->only(['index', 'update']);
+    });
+
 });
-
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
